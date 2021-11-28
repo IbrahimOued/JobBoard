1a662e661dd55a6758586c358c61643d6b18bb11
Ibrah <brahim.oued@gmail.com>
2021-11-28 14:52:59 +0000

    Add update and delete endpoints

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Installation of a postgresql database with docker
-```docker-compose -p jobboard up -d``
+```docker-compose -p jobboard up -d```
 
 Start the application in watch mode
 ```uvicorn main:app --reload```
--- backend/api/v1/route_jobs.py
+++ backend/api/v1/route_jobs.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -5,7 +6,7 @@ from sqlalchemy.orm import Session
 from db.session import get_db
 from db.models.jobs import Job
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job, retrieve_job
+from db.repository.jobs import create_new_job, delete_job_by_id, list_jobs, retrieve_job, update_job_by_id
 
 
 router = APIRouter()
@@ -25,3 +26,29 @@ def retrieve_job_by_id(id: int, db=Session):
         HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                       detail=f"Job with id {id} does not exist")
     return job
+
+
+@router.get('/all', response_model=List[ShowJob])
+def retrieve_all_jobs(db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return jobs
+
+
+@router.put('/update/{id}')
+def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does not exist")
+    return {"detail": "Successfully updated data"}
+
+
+@router.delete('/delete/{id}')
+def delete_job(id: int, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = delete_job_by_id(id=id, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} not found")
+    return {"detail": "Successfully deleted the job"}
